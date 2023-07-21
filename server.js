50dc29ccf8e2cb61c3b2aac16fea09f34bc60336
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2023-07-21 16:04:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ app.use('/api/jobs/' , jobsRoute)
 app.use('/api/users/' , userRoute)
 const port = process.env.PORT || 5000;
 
- if(process.env.NODE_ENV === 'production')
- {
-     app.use('/' , express.static('client/build'))
+ // if(process.env.NODE_ENV === 'production')
+ // {
+ //     app.use('/' , express.static('client/build'))
 
-     app.get("*", (req, res) => {
+ //     app.get("*", (req, res) => {
 
-          res.sendFile(path.join(__dirname, 'client/build/index.html'))
+ //          res.sendFile(path.join(__dirname, 'client/build/index.html'))
        
-     });
- }
+ //     });
+ // }
 
 
 app.listen(port, () => console.log('Node JS Server Started'));
