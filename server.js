e2c4c0ea0dd5a61c8bc6526cefdb82e6fbd55fa7
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2023-08-03 16:57:01 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,24 +2,30 @@ const express = require("express");
 const app = express();
 const db = require("./db.js");
 const path = require("path");
+const cors = require("cors");
+// const bodyParser = require("body-parser");
 const jobsRoute = require('./routes/jobsRoute')
 const userRoute = require('./routes/usersRoute')
-app.use(express.json())
-app.use('/api/jobs/' , jobsRoute)
 
-app.use('/api/users/' , userRoute)
+
+app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use('/api/jobs/', jobsRoute)
+
+app.use('/api/users/', userRoute)
 const port = process.env.PORT || 5000;
 
- // if(process.env.NODE_ENV === 'production')
- // {
- //     app.use('/' , express.static('client/build'))
+//  if(process.env.NODE_ENV === 'production')
+//  {
+//      app.use('/' , express.static('client/build'))
 
- //     app.get("*", (req, res) => {
+//      app.get("*", (req, res) => {
 
- //          res.sendFile(path.join(__dirname, 'client/build/index.html'))
+//           res.sendFile(path.join(__dirname, 'client/build/index.html'))
        
- //     });
- // }
+//      });
+//  }
 
 
 app.listen(port, () => console.log('Node JS Server Started'));
